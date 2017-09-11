3d641c16e67b4d71d045864423a62a385856820a
Student <student@Student-66.local>
2017-09-11 10:02:40 +0800

    Initial commit

---
+++ app/src/androidTest/java/cn/edu/gdmec/android/zhoumingpan/ExampleInstrumentedTest.java
@@ -0,0 +1,36 @@
+package cn.edu.gdmec.android.zhoumingpan;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+/**
+ * Instrumentation test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+    @Test
+    public void useAppContext() throws Exception {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext();
+
+        assertEquals("cn.edu.gdmec.android.zhoumingpan", appContext.getPackageName());
+    }
+    protected void onCreate(Bundle saveInstanceState){
+        super.onCreate(saveInstanceState);
+        setContentView(R.layout.activity_main);
+        if(1==1){
+            System.out.println("hi");
+
+        }
+
+    }
+}
